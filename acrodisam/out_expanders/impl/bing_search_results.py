623c4fd70a062c7ea5c4cd31192c6a7126b0397e
daneshlachman <danesh.lachman@student.uva.nl>
2023-06-11 16:07:32 +0200

    intermediate commit

--- acrodisam/out_expanders/impl/bing_search_results.py
+++ acrodisam/out_expanders/impl/bing_search_results.py
@@ -4,7 +4,8 @@ from inputters import TrainOutDataManager
 from text_preparation import *
 from .._base import OutExpanderFactory, OutExpanderArticleInput, OutExpander
 # kw_extracter = yake.KeywordExtracter()
-
+import statistics
+from statistics import mode
 import json
 from pprint import pprint
 from os import path
@@ -12,6 +13,7 @@ import requests
 import spacy
 import yake as yake
 from rake_nltk import Rake
+import string
 
 # import in-expanders SH, acx and maddog
 from AcroExpExtractors.AcroExpExtractor_Schwartz_Hearst import (
@@ -22,14 +24,11 @@ from AcroExpExtractors.AcroExpExtractor_MadDog import (
     AcroExpExtractor_MadDog,)
 
 
-cache_snippet_filepath = 'acrodisam/out_expanders/impl/search_results_cache.json'
+cache_snippet_filepath = 'acrodisam/out_expanders/impl/ScienceWISE_search_results.json'
 
 class FactorySearchEngine(OutExpanderFactory):
     def __init__(self, *args, **kwargs):
-        self.in_expander = AcroExpExtractor_Schwartz_Hearst()
-        self.acx_in_expander = AcroExpExtractor_Yet_Another_Improvement()
-        self.maddog_in_expander = AcroExpExtractor_MadDog()
-        cache_snippet_filepath = 'acrodisam/out_expanders/impl/search_results_cache.json'
+        self.type_in_expander = args
         pass
 
 
@@ -59,52 +58,20 @@ class FactorySearchEngine(OutExpanderFactory):
     def get_expander(
         self, train_data_manager: TrainOutDataManager, execution_time_observer=None
     ):
-        # raws_text = 'The meaning of PDF is a computer file format for the transmission of a multimedia document that is not intended to be edited further and appears unaltered in most computer environments; also : a document that uses this format. Portable Document Format (PDF), standardized as ISO 32000, is a file format developed by Adobe in 1992 to present documents, including text formatting and images, in a manner independent of application software, hardware, and operating systems. Based on the PostScript language, each PDF file encapsulates a complete description of a fixed-layout flat document, including the text, fonts, vector'
-        # expansion_dict = self.in_expander.get_all_acronym_expansion(raw_text)
-
-        if not path.isfile(cache_snippet_filepath):
-            raise Exception("File not found")
-        
-        with open(cache_snippet_filepath) as filepath:
-            search_results_jsonfile = json.load(filepath)
-        
-
-        # acronym_expansions = {}
-        # acronym_articles = {}
-        # execution_time_observer.start()
-        # for acronym, expansion_articles in train_data_manager.acronym_db.items():
-        #     distinct_expansions = []
-        #     distinct_articles = []
-        #     for exp_article in expansion_articles:
-        #         pdb.set_trace()
-        #         if not (exp_article[0] in (item for sublist in distinct_expansions for item in sublist)):
-
-        #             pdb.set_trace()
-        #             preproc_text = train_data_manager.get_preprocessed_articles_db()[exp_article[1]]
-        #             expansion_without_spaces = get_expansion_without_spaces(exp_article[0])
-        #             preproc_text_with_acronym = preproc_text.replace(expansion_without_spaces, acronym)
-        #             context = FactorySearchEngine.yake_as_context(acronym, preproc_text_with_acronym)
-        #             distinct_expansions.append([exp_article[0], context])
-
-        #     acronym_expansions[acronym] = distinct_expansions
-
-
-
-            execution_time_observer.stop()
 
             # pdb.set_trace()
-        return _ExpanderSearchEngine(search_results_jsonfile, train_data_manager)
+        return _ExpanderSearchEngine(self.type_in_expander)
 
 
 class _ExpanderSearchEngine(OutExpander):
-    def __init__(self, cached_contexts: dict, trainer):
-        self.cached_contexts = cached_contexts
-        self.trainer = trainer
+    def __init__(self, in_expander):
+        self.expander =  in_expander[0]
         self.sh_expander =  AcroExpExtractor_Schwartz_Hearst()
-        self.bing_factory = FactorySearchEngine()
+        self.acx_in_expander = AcroExpExtractor_Yet_Another_Improvement()
+        self.maddog_expander = AcroExpExtractor_MadDog()
 
     def perform_search_query(self, AcronymForSearchQuery, context):
-        subscription_key = "59dbcd200c134837adfdad83ead297cf"
+        subscription_key = "078e74d227da41d4ada52622eb655b9a"
         endpoint = "https://api.bing.microsoft.com/v7.0/search" 
 
         # Construct a request
@@ -134,25 +101,26 @@ class _ExpanderSearchEngine(OutExpander):
     def yake_as_context(self, acronym, text):
         kw_extracter = yake.KeywordExtractor(n=2)
         keywords = kw_extracter.extract_keywords(text)
-        # pdb.set_trace()
         for keyword in keywords:
             if acronym not in keyword[0]:
                 # print(keyword[0])
                 return keyword[0]
+            
+    def sh_expansion(self, acronym, text):
+        found_expansion = self.sh_expander.get_best_expansion(acronym, text)
+        return found_expansion
     
+    def maddog_expansion(self, acronym, text):
+        found_expansion = self.maddog_expander.get_best_expansion(acronym, text)
+        return found_expansion
+
     def process_article(self, out_expander_input: OutExpanderArticleInput):
         predicted_expansions = []
         article_id =  out_expander_input.article.article_id
         article_text = out_expander_input.article.get_raw_text()
-
-
-        # Receive list of acronyms which should be expanded
-        # loop through this list
-        # for each acronym get its context and apply in-expander on context to find acronym
-        # calculate some sort of probality
-        # return list expansions with probabilities
         
         for acronym in out_expander_input.acronyms_list:
+            predicted_expansions = []
             ## reopen file because intermediate change has potentially occured
             with open(cache_snippet_filepath) as filepath:
                 search_results_jsonfile = json.load(filepath)
@@ -166,22 +134,26 @@ class _ExpanderSearchEngine(OutExpander):
                     f.write(json.dumps(search_results_jsonfile, sort_keys=True, indent=4, separators=(',',  ': ')))
 
             elif article_id in list(search_results_jsonfile.keys()):
-
-                pdb.set_trace()
                 if acronym not in list(search_results_jsonfile[article_id].keys()):
-                    # pdb.set_trace()
                     context = self.yake_as_context(acronym, article_text)
                     bing_search_results = self.perform_search_query(acronym, context)
-                    # pdb.set_trace()
                     search_results_jsonfile[article_id][acronym] = bing_search_results
-                    print('Acronym added: ' + acronym + ' on article ' + article_id)
+                    print('Acronym added (with already existing article): ' + acronym + ' on article ' + article_id)
                     with open(cache_snippet_filepath,  'w') as f:
                         f.write(json.dumps(search_results_jsonfile, sort_keys=True, indent=4, separators=(',',  ': ')))
+            
+            # if article and acronym cached, perform in-expansion on the stored search results
+            concetenated_search_results = ''
+            for search_result in search_results_jsonfile[article_id][acronym]:
+                # search_result =  search_result.translate(str.maketrans('', '', string.punctuation))
+                concetenated_search_results += search_result
 
-        # for acronym in out_expander_input.acronyms_list:
-        #     expansions = self.acronym_expansion[acronym]
-        #     random_expansion = random.choice(expansions)
-        #     confidence = 1 / len(expansions)  
-        #     predicted_expansions.append((random_expansion, confidence))
-        #     pdb.set_trace()
+            # pdb.set_trace()
+            if self.expander == 'sh':
+                found_expansion = self.sh_expansion(acronym, concetenated_search_results)
+            elif self.expander == 'maddog':
+                found_expansion = self.maddog_expansion(acronym, concetenated_search_results)
+            predicted_expansions.append((found_expansion, 1))
+        print(predicted_expansions)
+        # pdb.set_trace()
         return predicted_expansions
